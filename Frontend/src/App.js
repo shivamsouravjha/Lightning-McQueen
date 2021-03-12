f1114109cff75efa4369af18b65ec95376404d2c
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-03-12 17:52:53 +0530

    Skeletone of Frontend Added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import Initialpage from './common/initialpage';
+import Attendee_register from './attendee/attendee_register';
+import Operation_Success from './common/sucess';
+import Get_Attendee from '../src/attendee/get_attendee';
+import Events_Work from './events/events';
+import Events_ID from './events/events_id';
+import Admin_login from '../src/admin/admin_login';
+import Create_Events from '../src/events/new_event';
+import Admin_Command from '../src/admin/admin_commands';
+import Delete_Event from '../src/events/deleteevent';
+import Operation_Fail from '../src/common/fail';
+const App= ()=> {
+  return(
+  <Router>
+    <Switch>
+    <Route path="/initial" exact>
+      <Initialpage />
+    </Route>
+    <Route path="/admin" exact>
+      <Admin_login />
+    </Route>
+    <Route path="/admin/newevents" exact>
+      <Create_Events />
+    </Route>
+    <Route path="/newattendee" exact>
+      <Attendee_register />
+    </Route>
+    <Route path="/registerevent" exact>
+      <Events_Work />
+    </Route>
+    <Route path="/getattendee" exact>
+      <Get_Attendee />
+    </Route>
+    <Route path="/fail" exact>
+      <Operation_Fail />
+    </Route>
+    <Route path="/admincommand" exact>
+      <Admin_Command />
+    </Route>
+    <Route path="/deleteevent" exact>
+    <Events_ID />
+    <Delete_Event />
+    </Route>
+    <Route path="/success" exact>
+      <Operation_Success />
+    </Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Redirect to="/initial"/>
+    </Switch>
+  </Router>
   );
 }
 
